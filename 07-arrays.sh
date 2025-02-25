7b302cb993d9c7c4bf26ef6dbddb6ca6e18c4d9f
UshaKondaka <jinagagiribabu@outlook.com>
2025-02-25 19:56:29 +0530

    shell script

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "rrr" "devara")
+
+echo "first movie: ${MOVIE[0]}"
+echo "second movie: ${MOVIE[1]}"
+echo "third movie: ${MOVIE[2]}"
+
+echo "all movies: ${MOVIES[@]}"
+
