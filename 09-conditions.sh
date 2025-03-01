ee40bd5a571fbef8b9936b3d2cc423c950365de3
UshaKondaka <jinagagiribabu@outlook.com>
2025-03-01 15:25:16 +0530

    script added

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+    if [$NUMBER -gt 100 ]
+    then 
+        echo "given number is greater than 100"
+    else 
+        echo "given number is less than or equal to 100"
+
+    fi
